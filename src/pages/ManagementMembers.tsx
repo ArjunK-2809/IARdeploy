f8fd69c07127866cb4f9ef4b28fb11e78e9734fd
Arjun-K-2809 <arjunkesavababu2005@gmail.com>
2025-02-05 15:25:08 +0530

    management_changes

--- src/pages/ManagementMembers.tsx
+++ src/pages/ManagementMembers.tsx
@@ -1,8 +1,67 @@
 import "./ManagementMembers.css"
+
+import Harshitha from "../assets/club/Harshitha.jpg"
+import Keerthana from "../assets/club/keerthana.jpg"
+import Liya from "../assets/club/Liya.jpg"
+import Vikranth from "../assets/club/Vikranth.jpg"
+
+
 const ManagementMembers = () => {
   return (
-      <div className="total">
-         The club members are under selection..................
+    <div>
+    
+    <div className="contact-page">
+    <h1>Management Team</h1>
+    <div className="contact-cards">
+      <div className="contact-card">
+        <div className="card-image">
+          <img src={Harshitha} alt="" />
+        </div>
+        <div className="card-content">
+          <h3>Harshitha</h3>
+          
+        </div>
+        </div>
+        <div className="contact-card">
+        <div className="card-image">
+          <img src={Keerthana} alt="" />
+        </div>
+        <div className="card-content">
+          <h3>Keerthana</h3>
+       
+        </div>
+        </div> 
+        
+      </div>
+    
+    </div>
+    
+    <div className="contact-page">
+    <div className="contact-cards">
+      <div className="contact-card">
+        <div className="card-image">
+          <img src={Liya} alt="" />
+        </div>
+        <div className="card-content">
+          <h3>Liya</h3>
+          
+        </div>
+        </div>
+        <div className="contact-card">
+        <div className="card-image">
+          <img src={Vikranth} alt="" />
+        </div>
+        <div className="card-content">
+          <h3>Vikranth</h3>
+       
+        </div>
+        </div> 
+        
+      </div>
+    
+    </div>
+    
+    
     </div>
   )
 }
